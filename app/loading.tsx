5728ed1521e7ed7ff389e4a618c1e4d30f3f809b
mostafakamal22 <36553185+mostafakamal22@users.noreply.github.com>
2023-07-09 23:53:29 +0300

    add loading state indicator

---
+++ app/loading.tsx
@@ -0,0 +1,21 @@
+import { CSSProperties } from "react";
+import { PuffLoader } from "react-spinners";
+
+export default function Loading() {
+  const override: CSSProperties = {
+    display: "block",
+    margin: "0 auto",
+  };
+
+  return (
+    <div className="absolute inset-0 w-full h-screen flex justify-center items-center bg-black/5">
+      <PuffLoader
+        color="#38bdf8"
+        cssOverride={override}
+        size={100}
+        aria-label="Loading Spinner"
+        data-testid="loader"
+      />
+    </div>
+  );
+}
